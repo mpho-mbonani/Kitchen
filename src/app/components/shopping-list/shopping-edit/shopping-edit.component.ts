82794738ee9ee9a589f1fca0c23d3a8a99e2a382
Mpho Mbonani <mmbonani@iqbusiness.net>
2021-09-16 16:19:43 +0200

    restructure

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredients.model';
+import { Ingredient } from 'src/app/models/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredients.model';
+import { Ingredient } from '../models/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
